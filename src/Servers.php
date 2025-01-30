cdb1c39ee5b5375bf0cb4a0e954c2ba0e929b9b9
Daniel Lievano <alexanderlievano99@gmail.com>
2025-01-29 20:41:48 -0500

    Se modifica la configuracion para que sea compatible con versiones inferiores de php.

--- src/Servers.php
+++ src/Servers.php
@@ -1,19 +1,19 @@
 <?php
-$serversConfig = [
-    [
+$serversConfig = array(
+    array(
         'host' => 'your_host',
         'port' => 'port',
         'username' => 'your_user_in_server',
         'privateKey' => 'directory_to_sshkey_or_ppk',
         'remote_directory' => 'remote_directory',
-    ],
-    [
+    ),
+    array(
         'host' => 'your_host',
         'port' => 'port',
         'username' => 'your_user_in_server',
         'pass' => 'your_password',
         'remote_directory' => 'remote_directory',
-    ]
-];
+    )
+    );
 $local_directory = 'c:/path_to_local_directory';
 ?>
